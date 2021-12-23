ca626c75b313566abe3496001aae28af214cb9b0
daeunKim <kimde1031@gmail.com>
2021-12-23 13:52:31 +0900

    Add algorithm 35

---
+++ Algorithm/programmers_1step/Algorithm35.js
@@ -0,0 +1,27 @@
+// 35. 예산
+
+// 풀이
+function solution(d, budget) {
+    let answer = 0;
+    let sum = 0;
+     
+    d.sort((a, b) => a - b);
+     
+    for (let i = 0; i < d.length; i++) {
+       answer++;
+       sum += d[i];
+ 
+       if (sum > budget) answer--;
+    }
+    
+    return answer;
+}
+
+// reduce() 사용해서 풀어보기
+function solution(d, budget) {
+    d.sort((a, b) => a - b);
+
+    while (d.reduce((a, b) => (a + b), 0) > budget) d.pop();
+
+    return d.length;
+}
