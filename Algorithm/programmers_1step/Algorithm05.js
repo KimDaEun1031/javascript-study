51ad0e106d290c3b38d46174b91dab1e698095d1
daeunKim <kimde1031@gmail.com>
2021-12-27 13:38:51 +0900

    Update algorithm 5

--- Algorithm/programmers_1step/Algorithm05.js
+++ Algorithm/programmers_1step/Algorithm05.js
@@ -21,3 +21,13 @@ function solution(s) {
 function solution(s){
     return s.toUpperCase().replace(/(\w)(\w)/g, function(a){return a[0].toUpperCase()+a[1].toLowerCase();})
 }
+
+
+// 다시 풀어보기 1 - 21/12/27
+function solution(s) { 
+  return s.split(' ').map((cur) => {
+    return cur.split('').map((current, index) => {      
+      return (index % 2 === 0 || index === 0) ? current.toUpperCase() : current.toLowerCase();
+    }).join('');   
+  }).join(' ');
+}
