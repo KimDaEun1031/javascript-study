eb635c7cde47767038114b832d98622e39547bda
daeunKim <kimde1031@gmail.com>
2021-12-29 11:21:55 +0900

    Update algorithm 7

--- Algorithm/programmers_1step/Algorithm07.js
+++ Algorithm/programmers_1step/Algorithm07.js
@@ -5,3 +5,8 @@ function solution(x) {
     var sum = (x+"").split("").reduce((acc, cur) => acc + parseInt(cur), 0)
     return x % sum === 0 ? true : false;         
 }
+
+// 다시 풀어보기 1 - 21/12/29
+function solution(x) {
+    return (x % String(x).split('').reduce((a, b) => a + Number(b), 0) === 0) ? true : false;
+}
