9fafedfdbcb549e8f036a9d0a904ed5f015f8623
daeunKim <kimde1031@gmail.com>
2022-01-18 21:41:00 +0900

    Add algorithm 60 Update algorithm 22

--- Algorithm/programmers_1step/Algorithm22.js
+++ Algorithm/programmers_1step/Algorithm22.js
@@ -5,3 +5,9 @@ function solution(n) {
     let x = Math.sqrt(n);     
     return Number.isInteger(x) ? Math.pow(x + 1, 2) : -1;
 }
+
+// 다시 풀어보기 1 - 22/01/18
+function solution(n) {
+    let num = Math.sqrt(n);
+    return Number.isInteger(num) ? Math.pow(num + 1, 2) : -1;
+}
